6e61050a172649ee1a7f67c82fee85d690667c74
lucas <mcbride.lucas9@gmail.com>
2016-10-19 11:39:08 -0400

    add bootstrap3

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'
+    'hello',
+    'bootstrap3',
+    # 'activelink',
 )
 
 MIDDLEWARE_CLASSES = (
