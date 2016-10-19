275ba717793a9b2bec70a9e383d59d79a4ac3cd1
lucas <mcbride.lucas9@gmail.com>
2016-10-19 11:38:23 -0400

    change base.html

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -5,12 +5,7 @@ admin.autodiscover()
 
 import hello.views
 
-# Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
-
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^', include('hello.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
