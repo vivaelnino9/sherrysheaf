b2fc567713295d520d30aec39ab2afb4ab42e970
lucas <mcbride.lucas9@gmail.com>
2016-10-19 11:54:57 -0400

    remove hello.views

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -3,8 +3,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
-
 urlpatterns = [
     url(r'^', include('hello.urls')),
     url(r'^admin/', include(admin.site.urls)),
